28017fb754637656a67826e80f8e012b49841105
Joshua Rostenne <joshua@theconsultant.net>
2022-10-23 21:49:08 -0400

    sections implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod particle;
 
-use particle::Particle;
+use std::time::Instant;
+use particle::{Particle, Sections};
 use nannou::prelude::*;
 
 fn main() {
+    // let mut a = Sections::new(20.);
+    // a.insert(Particle::new(0., 0., 10.));
+    // // a.insert(Particle::new(9., 9., 10.));
+    // // a.insert(Particle::new(10., 10., 10.));
+    // a.insert(Particle::new(11., 12., 10.));
+    // // a.insert(Particle::new(-1., 0., 10.));
+    // println!("{:#?}", a);
+    // let b = a.collision(&Particle::new(11., 10., 10.));
+    // println!("{:?}", b);
+
     nannou::app(model)
         .update(update)
         .run();
@@ -14,7 +25,7 @@ struct Model {
     particle_move_speed: f32,
     spawn_radius: f32,
     alive_particle_limit: usize,
-    dead_particles: Vec<Particle>,
+    dead_particles: Sections,
     particles: Vec<Particle>,
 }
 
@@ -27,35 +38,41 @@ fn model(app: &App) -> Model {
         particle_radius: 10.,
         particle_move_speed: 1.,
         spawn_radius: 600.,
-        alive_particle_limit: 1,
-        dead_particles: vec![Particle::new(0., 0.)],
+        alive_particle_limit: 200,
+        dead_particles: {
+            let mut sections = Sections::new(10.);
+            sections.insert(Particle::new(0., 0., 10.));
+            sections
+        },
         particles: vec![],
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    for _ in 0..1 {
+    for _ in 0..100 {
         for _ in 0..(model.alive_particle_limit - model.particles.len()) {
-            let p = Particle::new_on_radius(model.spawn_radius);
+            let p = Particle::new_on_radius(model.spawn_radius, model.particle_radius);
             model.particles.push(p);
         }
 
         let mut removed = vec![];
-        while removed.len() == 0 {
+        // while removed.len() == 0 {
+            let start = Instant::now();
             for e in model.particles.iter_mut().enumerate() {
                 let (n, p): (usize, &mut Particle) = e;
-                if p.collision(&model.dead_particles, model.particle_radius) {
-                    model.dead_particles.push(p.clone());
+                if model.dead_particles.collision(p) {
+                    model.dead_particles.insert(p.clone());
                     removed.push(n);
                 } else {
                     let win = app.window_rect();
                     p.update(win, model.particle_move_speed);
                 }
             }
-        }
+            println!("collision check: {:?}", start.elapsed());
+        // }
         for removed_point_index in removed {
             model.particles.remove(removed_point_index);
-            model.particles.push(Particle::new_on_radius(model.spawn_radius));
+            model.particles.push(Particle::new_on_radius(model.spawn_radius, model.particle_radius));
         }
     }
 }
@@ -64,11 +81,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     draw.background().color(BLACK);
-    // for p in &model.particles {
-    //     draw.ellipse().x_y(p.get_pos().x, p.get_pos().y).w_h(model.particle_radius, model.particle_radius).color(WHITE);
-    // }
-    for p in &model.dead_particles {
-        draw.ellipse().x_y(p.get_pos().x, p.get_pos().y).w_h(model.particle_radius, model.particle_radius).color(AQUA);
+    for p in &model.particles {
+        draw.ellipse().x_y(p.get_pos().x, p.get_pos().y).w_h(p.get_radius(), p.get_radius()).color(WHITE);
+    }
+    for p in model.dead_particles.all_particles() {
+        draw.ellipse().x_y(p.get_pos().x, p.get_pos().y).w_h(p.get_radius(), p.get_radius()).color(AQUA);
     }
 
     draw_fps(app, &draw);
--- src/particle.rs
+++ src/particle.rs
@@ -1,24 +1,114 @@
 use nannou::prelude::*;
+use std::collections::HashMap;
 
-#[derive(Clone)]
-#[derive(Debug)]
-pub(crate) struct Particle {
+#[derive(Clone, Debug)]
+pub struct Particle {
     pos: Vec2,
+    radius: f32,
 }
 
-impl Particle {
-    pub(crate) fn new(x: f32, y: f32) -> Particle {
-        Particle {
-            pos: vec2(x, y),
+#[derive(Debug)]
+pub struct Sections {
+    sections: HashMap<Vec<i32>, Section>,
+    size: f32,
+}
+
+#[derive(Debug)]
+struct Section {
+    particles: Vec<Particle>,
+}
+
+impl Sections {
+    pub fn new(size: f32) -> Self {
+        Sections {
+            sections: HashMap::new(),
+            size,
         }
     }
 
-    pub(crate) fn new_on_radius(radius: f32) -> Particle {
-        let angle = random::<f32>() * PI * 2.;
-        Particle::new(radius * angle.cos(), radius * angle.sin())
+    pub fn insert(&mut self, particle: Particle) {
+        let row = (particle.get_pos().x / self.size).floor() as i32;
+        let col = (particle.get_pos().y / self.size).floor() as i32;
+        match self.sections.get_mut(&vec![row, col]) {
+            Some(section) => {
+                section.particles.push(particle);
+            }
+            None => {
+                self.sections.insert(
+                    vec![row, col],
+                    Section::new(vec![particle])
+                );
+            }
+        }
     }
 
-    pub(crate) fn update(&mut self, win: Rect, move_speed: f32) {
+    pub fn collision(&self, particle: &Particle) -> bool {
+        for section in self.get_surrounding_sections(particle) {
+            if section.collision(particle) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn get_section_key(&self, particle: &Particle) -> (i32, i32) {
+        let row = (particle.get_pos().x / self.size).floor() as i32;
+        let col = (particle.get_pos().y / self.size).floor() as i32;
+        (row, col)
+    }
+    fn get_surrounding_sections(&self, particle: &Particle) -> Vec<&Section> {
+        let center_key = self.get_section_key(particle);
+        let mut surrounding_sections = vec![];
+        for x in -1..=1 {
+            for y in -1..=1 {
+                match self.sections.get(&vec![center_key.0 + x, center_key.1 + y]) {
+                    Some(section) => {
+                        surrounding_sections.push(section);
+                    }
+                    None => (),
+                }
+            }
+        }
+        surrounding_sections
+    }
+
+    pub fn all_particles(&self) -> Vec<&Particle> {
+        let mut particles = vec![];
+        for section in self.sections.values() {
+            for particle in section.particles.iter() {
+                particles.push(particle);
+            }
+        }
+        particles
+    }
+}
+
+impl Section {
+    fn new(particles: Vec<Particle>) -> Self {
+        Section {
+            particles
+        }
+    }
+
+    fn collision(&self, particle: &Particle) -> bool {
+        particle.collision(&self.particles)
+    }
+}
+
+impl Particle {
+    pub fn new(x: f32, y: f32, radius: f32) -> Particle {
+        Particle {
+            pos: vec2(x, y),
+            radius
+        }
+    }
+
+    pub fn new_on_radius(radius: f32, particle_radius: f32) -> Particle {
+        let angle = random::<f32>() * PI * 2.;
+        Particle::new(radius * angle.cos(), radius * angle.sin(), particle_radius)
+    }
+
+    pub fn update(&mut self, win: Rect, move_speed: f32) {
         if random::<f32>() < 0.5 {
             self.pos.x += move_speed;
         } else {
@@ -33,18 +123,20 @@ impl Particle {
         self.pos.y = clamp(self.pos.y, win.top(), win.bottom());
     }
 
-    pub(crate) fn collision(&mut self, points: &Vec<Particle>, radius: f32) -> bool {
+    fn collision(&self, points: &Vec<Particle>) -> bool {
         for p in points {
-            if p.pos.distance_squared(self.pos) < radius * radius {
+            if p.pos.distance_squared(self.pos) < self.radius * self.radius {
                 return true;
             }
         }
         false
     }
-}
 
-impl Particle {
     pub fn get_pos(&self) -> Vec2 {
         self.pos
+    }
+
+    pub fn get_radius(&self) -> f32 {
+        self.radius
     }
 }
